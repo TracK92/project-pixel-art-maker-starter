af9dd2fbd4f834eb95d8ec6e10551c649ca87db5
TracK92 <40690789+TracK92@users.noreply.github.com>
2018-07-01 03:51:44 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -5,6 +5,24 @@
 
 function makeGrid() {
 
-// Your code goes here!
-
-}
+$document).ready(function() {
+  $('#sizePicker').submit(function makeGrid(grid) {
+    $('table tr').remove();
+    var rows = $('#input_height').val();
+    var columns = $('#input_width').val();
+      for (var x=1; x<= rows; x++) {
+        $('table'). append('<tr></tr>');
+        for (var y=1; y<= columns; y++) {
+          $('tr.last').append('<td></td>');
+          $('td').attr("class", 'cells');
+        }
+      }
+    grid.preventDefault();
+    
+    $('.cells').click(function(event) {
+      var color = $('colorPicker').val();
+      $(event.target().css('background-color', color);
+    })
+  })
+  });
+                          
